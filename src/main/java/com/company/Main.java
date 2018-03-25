9e4dccafb1fb950dd749dd3cb13d9b297dcf352b
Ben Slabbert <ben@hex.co.za>
2018-03-25 19:37:20 +0200

    cleaning uo

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,31 +6,24 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 class Main {
-    public static void main(String[] args) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pets", "root", "root")) {
-                String sql = "INSERT INTO pet (name, age, type) values (?, ?, ?)";
-                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                    stmt.setString(1, "ben");
-                    stmt.setInt(2, 1);
-                    stmt.setString(3, "dog");
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pets", "root", "root")) {
+            String sql = "INSERT INTO pet (name, age, type) values (?, ?, ?)";
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, "ben");
+                stmt.setInt(2, 1);
+                stmt.setString(3, "dog");
 
-                    int numberOfUpdates = stmt.executeUpdate();
-                    System.out.println("This must be 1 : " + numberOfUpdates);
-                }
+                int numberOfUpdates = stmt.executeUpdate();
+                System.out.println("This must be 1 : " + numberOfUpdates);
+            } catch (SQLException e) {
+                System.err.println("SQL exception: " + e.getCause());
+                e.printStackTrace();
             }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
+            System.err.println("SQL exception: " + e.getCause());
             e.printStackTrace();
         }
     }
-}
-
-class JDBCExample {
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/pets2";
-    static final String USER = "root";
-    static final String PASS = "";
 }
